4acaf2e5e559c454f09ae3f022bf4446d33c3313
notrodans <notrodans@gmail.comm>
2023-03-19 17:11:42 +0200

    seeder

---
+++ seeder.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+const createProducts = async (quantity: number) => {
+	let productsLength = 0;
+	for (let i = 0; i < quantity; i++) {
+		const productTitle = faker.commerce.productName();
+		const categoryName = faker.commerce.department();
+
+		try {
+			await prisma.product.create({
+				data: {
+					title: productTitle,
+					slug: faker.helpers.slugify(productTitle),
+					description: faker.commerce.productDescription(),
+					price: +faker.commerce.price(10, 4999, 0),
+					primaryImage: faker.image.imageUrl(),
+					rating: +faker.datatype.float({ min: 1, max: 5 }).toFixed(1),
+					images: Array.from({ length: 2 }).map(() => faker.image.imageUrl()),
+					brand: "apple",
+					category: {
+						create: {
+							image: faker.image.imageUrl(),
+							name: categoryName,
+							slug: faker.helpers.slugify(categoryName)
+						}
+					}
+				}
+			});
+			productsLength += 1;
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	console.log(productsLength);
+};
+
+async function main() {
+	console.log("Start seeding");
+	await createProducts(10);
+}
+
+main()
+	.catch(e => console.log(e))
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
